a923498c1686350e7fc41292ac6bee640f438c91
CameronConrad <143106570+CameronConrad@users.noreply.github.com>
2023-09-06 09:16:42 -0500

    Completed cereal project

--- cereal.py
+++ cereal.py
@@ -1,20 +1,56 @@
 # 09/06/23
-# Class Chart
+# Class Charthttps://kleki.com/
 
 def format(name, grade, age, favCereal):
-    print("{}\t{}\t{}\t{}".format(name, grade, age, favCereal))
+    print("{}\t\t{}\t\t{}\t\t{}".format(name, grade, age, favCereal))
 
 print("Mrs. Goodman's Python Class")
 print("--------------------------------------------------------")
 print("Name:\tGrade:\tAge:\tFavorite Cereal:")
+
+# Christian's table
 format("Christian", 11, 16, "Fruity Pebbles")
+format("Jace", 12, 17, "Frosted Flakes")
+format("Eli", 11, 16, "Fruity Pebbles")
+format("Mya", 11, 17, "Frosted flakes")
+format("Brandon", 11, 16, "Captain Crunch")
 
-data = []
+#Lauren's Table
+format("Lauren", 11, 16, "Lucky Charms")
+format("Callie", 12, 17, "Fruity Pebbles")
+format("Tessa", 12, 17, "Dry Frosted Flakes")
 
-for i in range(16):
-    name, grade, age, favCereal = eval(input())
-    data.append(format(data))
+# Thomas's Table
+format("Thomas", 12, 17, "Cinnamon Toast Crunch")
+format("Isaiah", 12, 18, "Krave")
+format("Cody", 11, 16, "Fruit Loops")
+format("Jude", 11, 16, "Captain Crunch")
+format("Roman", 12, 17, "Cheerios")
 
-for item in data:
-    # You don't want to type in everything in the input, trust me, just have it hard coded
-    print(item)
+# Nate's Table
+format("Devan", 11, 16, "Frosted Flakes")
+format("Nathaniel", 11, 16, "Strawberry Frosted Mini Wheats")
+format("Mason", 12, 18, "Reese's Puffs")
+format("Miles", 11, 16, "Cinnamon Toast Crunch")
+format("Dawson", 11, 17, "Cap'n Crunch")
+
+#Andy's table
+format("Andy",12, 18,"none")
+format("Paul",11,16,"Cinnamon Toast Crunch")
+format("Jayden",11,16,"Fruit Pebbles")
+
+
+#Javier's table
+format("Wesley",11, 16, "rice crispies")
+format("Talan",11, 16, "Fruit loops")
+format("Kendle",12, 17, "Fruity pebles")
+format("Javier",11, 15, "rice crispies")
+
+#Cameron's table
+format("Grant", 12, 17, "Cinnamon Toast Cruch")
+format("Elias", 12, 17, "Cinnamon Toast Crunch")
+format("Cooper", 12, 17, "Cinnamon Toast Crunch")
+format("Christian", 12, 18, "Cinnamon Toast Crunch")
+format("Grant", 12, 18, "Honeynut Cherios")
+format("Kieran",12,17,"Cinnamon Toast Crunch")
+format("Tony", 12, 17, "Cinnamon Toast Crunch")
